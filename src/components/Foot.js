fc7400968d4927761d09de54b4e0010bf215bc1e
Leo <935987+kn18ht30us@users.noreply.github.com>
2023-05-24 19:54:40 +0100

    Oxara - add scaffolding

---
+++ src/components/Foot.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Foot = () => {
+  return (
+    <footer class="navbar bg-light">
+      <div class="container">
+        <small class="my-2 text-uppercase text-dark">&copy;2023 &middot; Zecco</small>
+      </div>
+    </footer>
+  )
+}
+
+export default Foot
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <nav class="navbar bg-light">
+      <div class="container">
+        <Link to={'/'} class="navbar-brand">Zecco</Link>
+
+        <div className='d-flex'>
+
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
